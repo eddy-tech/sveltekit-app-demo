9ff1ce4a4572e58ef902d99134c79b27f42f342f
ezafackkenfack <ezafackkenfack@sedona.fr>
2023-07-27 19:13:57 +0200

    architecture project sveltekit

---
+++ src/routes/product/[id]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { page } from "$app/stores";
+    import ProductHeader from "../ProductHeader.svelte";
+
+    $: console.log(page);
+</script>
+
+<ProductHeader />
+
+<p> Product ID: {$page.params.id}</p>
+
+<h1>Product Page</h1>
